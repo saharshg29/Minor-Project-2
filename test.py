a975c0b2a1703ce5761b5adcddda66dbe1f981ae
Saharsh Gupta <saharshg29@gmail.com>
2024-04-24 23:00:10 +0530

    better ui

--- test.py
+++ test.py
@@ -18,27 +18,6 @@ def preprocess_image(image):
     image_np = np.expand_dims(image_np, axis=0)  # Add batch dimension
     return image_np
 
-# def preprocess_image(image):
-    # Convert to grayscale
-    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    
-    # Resize to 224x224
-    resized_image = cv2.resize(gray_image, (IMG_SIZE, IMG_SIZE))
-    
-    # Apply CLAHE (Contrast Limited Adaptive Histogram Equalization) for better contrast
-    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-    clahe_image = clahe.apply(resized_image)
-    
-    # Normalize the image
-    normalized_image = clahe_image / 255.0
-    
-    # Add channel dimension and batch dimension
-    image_np = np.expand_dims(normalized_image, axis=-1)
-    image_np = np.expand_dims(image_np, axis=0)
-    
-    return image_np
-
-
 # Function to make predictions
 def predict(image):
     preprocessed_image = preprocess_image(image)
@@ -50,21 +29,25 @@ def predict(image):
 # Streamlit app
 def main():
     st.title("Diabetic Retinopathy Prediction App")
+    st.markdown("---")
     st.write("Upload an image of an eye to predict the presence of diabetic retinopathy.")
+    st.markdown("---")
 
     # File uploader
     uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "png"])
 
     if uploaded_file is not None:
         # Display the uploaded image
-        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        image = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), 1)
         st.image(image, caption='Uploaded Image', use_column_width=True)
 
         # Make prediction on the uploaded image
         if st.button("Predict"):
             max_prediction, max_index = predict(image)
-            st.write(f"Maximum Prediction Value: {max_prediction}")
-            st.write(f"Predicted Class: {max_index}")
+            st.markdown("---")
+            st.write("## Prediction Results")
+            st.write(f"Maximum Prediction Value: **{max_prediction:.2%}**")
+            st.write(f"Predicted Class: **Class {max_index}**")
 
 if __name__ == "__main__":
     main()
